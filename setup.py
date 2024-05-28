93bad9c2e626cdd9f3a06909c84d10ea6009f8f8
pratheeshrussell-qb <pratheesh.r@qburst.com>
2024-05-28 16:42:07 +0530

    setup update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in whitetheme_v13/__init__.py
-from business_theme import __version__ as version
+from frapee_business_theme import __version__ as version
 
 setup(
 	name='frapee_business_theme',
