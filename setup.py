0b450b4f3a0e624fe3dadfec3f3ef7b74647af8a
Pratheesh Russell <109942851+pratheeshrussell-qb@users.noreply.github.com>
2024-05-28 16:36:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import re, ast
 
 with open('requirements.txt') as f:
-    install_requires = f.read().strip().split('\n')
+	install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in business_theme/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('business_theme/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+# get version from __version__ variable in whitetheme_v13/__init__.py
+from business_theme import __version__ as version
 
 setup(
-	name='business_theme',
+	name='frapee_business_theme',
 	version=version,
-	description='General theme for frappe',
-	author='Randy Lowery',
-	author_email='randy.lowery@hotmail.com',
+	description='Business theme for v13',
+	author='Pratheesh',
+	author_email='pratheeshrussell@gmail.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires=install_requires,
+	install_requires=install_requires
 )
